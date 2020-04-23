cadf6aeab239f7cfceb93ba8332efd7308eecb3a
liujianyong <1260886176@qq.com>
2020-04-23 14:17:57 +0800

    第一次提交

---
+++ src/main/java/com/ljy/util/StringUtil.java
@@ -0,0 +1,5 @@
+package com.ljy.util;
+
+public class StringUtil {
+
+}
---
+++ src/test/java/com/ljy/util/RandomUtilTest.java
@@ -0,0 +1,29 @@
+package com.ljy.util;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class RandomUtilTest {
+
+	@Test
+	void testRandom() {
+		System.out.println(RandomUtil.random(1, 10));
+	}
+
+	@Test
+	void testSubRandom() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testRandomCharacter() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testRandomString() {
+		fail("Not yet implemented");
+	}
+
+}
